cb47a6853e83c28863c3ec0158656e51082de5ed
gyumingim <a31620504@gmail.com>
2024-11-25 14:47:02 +0900

    2,3 층 추가

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from fastapi.staticfiles import StaticFiles
 import paho.mqtt.client as mqtt
 import time
 import asyncio
-import paho.mqtt.client as mqtt
+# import paho.mqtt.client as mqtt
 import time  # 메시지 전송 간격을 위해 추가
 import atexit
 # Broker address
 broker_address = "10.150.151.204"
-client1 = mqtt.Client("python_pub_1")  # 발행자 역할을 반영하도록 이름 변경
+# client1 = mqtt.Client("python_pub_1")  # 발행자 역할을 반영하도록 이름 변경
 
 app = FastAPI()
 
@@ -17,22 +17,22 @@ app = FastAPI()
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
-client1.on_connect = on_connect
+# client1.on_connect = on_connect
 
 # # Connect to broker
-client1.connect(broker_address, 1883)
-client1.loop_start()  # 백그라운드에서 네트워크 루프 시작
+# client1.connect(broker_address, 1883)
+# client1.loop_start()  # 백그라운드에서 네트워크 루프 시작
 
 # 메시지 발행 루프
 def send_direction(direction):
-    client1.publish("jetson/jetson", direction)
+    # client1.publish("jetson/jetson", direction)
     print(f"Published: {direction}")
 
 
 def handle_exit():
     print("발행자 종료")
-    client1.loop_stop()
-    client1.disconnect()
+    # client1.loop_stop()
+    # client1.disconnect()
 
 @app.get("/aaaaa")
 def pub(topic_str="jetson/jetson", message_str="hello world"):
